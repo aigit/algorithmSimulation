55be5f77eedaa652af0f7127865f7fdab032b080
liudelong <ldl_V_010823>
2022-06-28 21:47:23 +0800

    添加算法

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,10 @@
+import org.problems.DigitsCombins;
+
+public class MyTest {
+
+    public static void main(String[] args) {
+        DigitsCombins digitsCombins = new DigitsCombins();
+        digitsCombins.test("235");
+    }
+
+}
---
+++ src/main/java/org/problems/DigitsCombins.java
@@ -0,0 +1,58 @@
+package org.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DigitsCombins {
+
+
+    static {
+
+    }
+
+    public void test(String digits){
+        List<String> list = letterCombinations(digits);
+        list.forEach(System.out::println);
+    }
+
+    public List<String> letterCombinations(String digits) {
+
+        List<String>list=new ArrayList<>();
+        String []s=new String[digits.length()];
+        int M=digits.length();
+        if(s.length==0){
+            return list;
+        }
+        for(int i=0;i<digits.length();i++){
+            switch (digits.charAt(i)){
+                case '2':s[i]="abc";break;
+                case '3':s[i]="def";break;
+                case '4':s[i]="ghi";break;
+                case '5':s[i]="jkl";break;
+                case '6':s[i]="mno";break;
+                case '7':s[i]="pqrs";break;
+                case '8':s[i]="tuv";break;
+                case '9':s[i]="wxyz";break;
+            }
+        }
+        list=getStringWithFor(s,0,list,"");
+        return list;
+    }
+
+    private static List<String> getStringWithFor(String []s,int i,List<String> list,String stemp) {
+
+        if(i<s.length-1){
+            for(int j=0;j<s[i].length();j++){
+                list=getStringWithFor(s,i+1,list,stemp+s[i].charAt(j));
+            }
+        } else {
+            for(int j=0;j<s[i].length();j++){
+                list.add(stemp+s[i].charAt(j));
+            }
+        }
+
+        return list;
+    }
+
+}
